ee36c7a1ff9cb62423cefddb463e4e437e67c293
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-01-16 19:49:26 +0300

    1 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,20 @@
 function factorial(n) {
-  // ваш код...
+  if ((n == 1) || (n == 0)) {
+    return 1;
+  }
+  else if (n < 0) {
+    return 0;
+  }
+  else{
+    let f = n;
+    let k = 1;
+    while ( k != n){
+      f = f*(n-k);
+      k++;
+    }
+    return f;
+  }
 }
+
+
+
