ce3aa85f27a3b79e151266c1e128f9b5b35d86ee
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-01-17 20:28:24 +0300

    4 task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let str1 = str.toLowerCase();
+  if ((str1.includes("1xbet") == 1) || (str1.includes("xxx") == 1)){
+      return true;
+      }
+  else{
+    return false;
+  }
 }
