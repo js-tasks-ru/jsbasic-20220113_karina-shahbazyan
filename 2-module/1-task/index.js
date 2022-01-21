6430c69b4cc7e26cec2a9664e2533f78952b362e
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-01-21 10:51:17 +0300

    1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+     if (salaries.Ann != undefined && salaries.John != undefined && salaries.Pete != undefined){
+    return salaries.John + salaries.Ann + salaries.Pete;
+  }
+  else{
+    return 0;
+  }
 }
