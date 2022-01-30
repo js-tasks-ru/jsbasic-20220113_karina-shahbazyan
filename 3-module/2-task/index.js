36760824b1a6fcfcbcd8157292ac4f27838aba59
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-01-30 21:28:11 +0300

    2 task upgrade

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,6 +1,4 @@
 function filterRange(arr, a, b) {
   const rangeFilter = arr.filter((item) => item >= a && item <= b);
-  const newArr = rangeFilter.map((item) => item);
-  
-  return newArr;
+  return rangeFilter.map((item) => item);
 }
