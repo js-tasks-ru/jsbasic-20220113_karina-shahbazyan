462e0d1c865c1ba2753985d3015494fb715bba7f
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-01-30 22:30:42 +0300

    3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  let name;
+  for (let index = 1; index < arr.length; index++) {
+     name = arr[index];
+     name = name[0].toUpperCase() + name.slice(1, name.length)
+     arr[index] = name;
+  }
+  return arr.join('');
 }
