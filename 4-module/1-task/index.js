4867f366e4d9af07ba70ae3158aaf8957afeb217
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-02-03 17:56:09 +0300

    1 task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ulElement = document.createElement("ul");
+  friends = friends.map((friend) => friend.firstName + ' ' + friend.lastName);
+  for (let key in friends) {
+    let liElement = document.createElement("li");
+    liElement.append(friends[key]);
+    ulElement.append(liElement);
+  }
+  return ulElement;
 }
