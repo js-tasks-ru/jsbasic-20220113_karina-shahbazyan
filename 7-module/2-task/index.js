26a8bd076e4ece6ccc7a5cc19e75b4f51090a7bb
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-02-19 11:38:38 +0300

    module 7

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,47 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = createElement(`
+    <div class="modal">
+      <div class="modal__overlay"></div>
+      <div class="modal__inner">
+        <div class="modal__header">
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+          <h3 class="modal__title"></h3>
+        </div>
+        <div class="modal__body"></div>
+      </div>
+    </div>`)
+    this.title = this.elem.querySelector('.modal__title');
+    this.body = this.elem.querySelector('.modal__body');
+  }
+  setTitle(title) {
+    this.title.innerText = title;
+
+  }
+  setBody(node) {
+    this.body.innerHTML = node.outerHTML;
+  }
+  open() {
+    document.body.append(this.elem);
+    document.body.classList.add('is-modal-open');
+    let closeButton = document.body.querySelector('.modal__close');
+    closeButton.addEventListener('click', this.close);
+    document.addEventListener('keydown', this.#keyDown)
+  }
+  #keyDown = (event) => {
+    if (event.code === "Escape") {
+      this.close();
+    }
+  }
+  close() {
+    
+    let modal = document.querySelector(".modal");
+    if (modal !== null) {
+      modal.remove();
+      document.body.classList.remove('is-modal-open');
+    }
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,68 @@
+
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = `<span></span>\n`.repeat(steps);
+    this.elem = document.createElement('div');
+    this.elem.classList.add('slider');
+    const stepSliderContent = this.#templateForStepSlider(this.steps);
+    this.elem.innerHTML = stepSliderContent;
+    this.arrayOfSteps = Array.from(this.elem.querySelectorAll('span'));
+    this.arrayOfSteps[1].classList.add('slider__step-active');
+    this.sliderValue = this.elem.querySelector('.slider__value');
+    this.sliderValue.innerText = value;
+    this.progress = this.elem.querySelector('.slider__progress');
+    this.progress.style.width = `${0}%`
+    this.#stepSliderChanger();
+  }
+
+  #stepSliderChanger() {
+    this.elem.addEventListener('click', this.#stepSliderOnClick)
+  }
+
+  #stepSliderOnClick = (event) => {
+    let thumb = this.elem.querySelector('.slider__thumb');
+    let progress = this.elem.querySelector('.slider__progress');
+    let left = event.clientX - this.elem.getBoundingClientRect().left;
+    let leftRelative = left / this.elem.offsetWidth;
+    let segments = this.arrayOfSteps.length - 2;
+    let approximateValue = leftRelative * segments;
+    let value = Math.round(approximateValue);
+    let valuePercents = value / segments * 100;
+    this.sliderValue.innerText = value;
+
+    this.arrayOfSteps.forEach((item) => { item.classList.remove('slider__step-active')});
+    this.arrayOfSteps[value+1].classList.add('slider__step-active');
+      
+    thumb.style.left = `${valuePercents}%`;
+    progress.style.width = `${valuePercents}%`;
+    const stepEvent = new CustomEvent('slider-change', { 
+      detail: value, 
+      bubbles: true 
+    })
+    this.elem.dispatchEvent(stepEvent);
+
+      
+  } 
+  
+  #templateForStepSlider(steps) {
+    return `
+    
+
+      <!--Ползунок слайдера с активным значением-->
+      <div class="slider__thumb">
+        <span class="slider__value">0</span>
+      </div>
+
+      <!--Полоска слайдера-->
+      <div class="slider__progress"></div>
+
+      <!-- Шаги слайдера (вертикальные чёрточки) -->
+      <div class="slider__steps">
+        <!-- текущий выбранный шаг выделен этим классом -->
+        ${steps}
+      </div>
+    
+    `;
   }
 }
