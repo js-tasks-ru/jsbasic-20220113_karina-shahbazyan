ea94656eb58944c4e3757ee64e7bba27d3147b36
Karina Shahbazyan <karina.shakhbazyan@gmail.com>
2022-02-19 21:11:03 +0300

    1 task

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -39,6 +39,27 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (!this.elem.offsetWidth) return; 
+  if (window.pageYOffset > 50) {
+    let leftIndent = Math.min(
+      document.querySelector('.container').getBoundingClientRect().right + 20,
+      document.documentElement.clientWidth - this.elem.offsetWidth - 10
+    ) + 'px'
+    Object.assign(this.elem.style, {
+      position: 'fixed',
+      top: '50px',
+      zIndex: 1e3,
+      right: '10px',
+      left: leftIndent
+    });
+    
+  } else {
+    Object.assign(this.elem.style, {
+      position: '',
+      top: '',
+      left: '',
+      zIndex: ''
+    });
+  }
   }
 }
